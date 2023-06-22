754199b7c0e8868809389dd86284e7a866e532ce
Alkaison <505ganeshmourya@gmail.com>
2023-06-22 22:31:36 +0530

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+/* const qrCodeImg = document.querySelector(".qrcode-img");
+
+var qr = new QRCode(qrCodeImg, "https://github.com/Alkaison");
+
+var qr = new QRCode(qrCodeImg, "Noob Dev");
+
+var qr = new QRCode(qrCodeImg, {
+	text: "yo guys!",
+	width: 128,
+	height: 128,
+	colorDark : "#000000",
+	colorLight : "#ffffff",
+	correctLevel : QRCode.CorrectLevel.H
+});
+
+// console.log(qr);
+
+qr.clear; // clear the code.
+
+qr.makeCode("http://olopo.com"); // make another code.
+
+new QRCode(document.getElementById("qrcode"), "http://jindo.dev.naver.com/collie"); */
